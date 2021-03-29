1512fd06e982b8f6576ee954541691f6f5d55b84
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-03-29 17:47:54 +0900

    Day1

--- Hello/src/com/javalec/base/MainClass.java
+++ Hello/src/com/javalec/base/MainClass.java
@@ -3,8 +3,46 @@ package com.javalec.base;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+//		화면에 문자열 상수 출력
+		System.out.println("------------");
+		System.out.println("Hello, World");
+		System.out.println("------------");
+		
+//		화면에 정수(숫자) 상수 출력
+		System.out.println(10);
+		System.out.println(10 + 20);
+		System.out.println(10 - 20);
+		System.out.println(10 * 20);
+		
+		
+//		나눗셈을 할떄는 범위를 정해서 보내주기 때문에 정수와 정수를 나면 정수를 반환, 실수와 실수를 나누면 실수를 반환
+		 
+		System.out.println(10 / 20);
+		System.out.println(10.0 / 20.0);
+		
+//		정수와 실수를 실수와 정수를 하게 된다면 실수를 반환하게 된다.
+		
+		System.out.println(10/20.0);
 
+		
+//		변수 선언
+		int a = 10;
+		int b = 20;
+//		형변환 int -> float
+		double afloat = (double)a;
+		double bfloat = (double)b;
+				
+		
+		System.out.println(a+"+"+b+"="+a+b);
+		System.out.println(a+"-"+b+"="+(a-b));
+		System.out.println(a+"*"+b+"="+(a*b));
+		System.out.println(a+"/"+b+"="+(a/b));
+		System.out.println(afloat+"/"+bfloat+"="+(afloat/bfloat));
+		
+		
+		
+		
 	}
 
 }
---
+++ Variable_01/src/com/javalec/base/Variable_01.java
@@ -0,0 +1,62 @@
+package com.javalec.base;
+
+public class Variable_01 {
+
+	public static void main(String[] args) {
+//		변수(Variable)
+//		문자열 변수 선언
+		
+//		data type , 변수 이름 =(-> 할당 연산자) 데이터
+		String hello = "Hello World";
+		
+		System.out.println(hello);
+		System.out.println(hello);
+		
+		hello = "GoodBye World";
+		
+//		변수 이름을 만들기 위해서는 Camel Cascading 
+//		타입은 소문자 이지만, String 문자열 (클라스형태) 이것만 대문자로 시작--> 나중에 업데이트 됨
+//		정수 변수 선언
+		int num1 = 10;
+		int num2 = 20;
+		
+//		선언 부분은 위에 있는게 무조건 좋다 ! 
+		int plusResult;
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println((double)num1 / num2);
+		
+		System.out.println((double)(num1 / num2));// 변환 의미 없음
+		
+		plusResult = num1 + num2;
+		
+		num1 = 100;
+		
+		System.out.println(plusResult);
+		
+		
+//		------Exercise------
+		
+		int startNum = 30, endNum = 40;
+		
+//		30과 40의 덧셈의 결과는 70 입니다.
+//		30과 40의 뺄셈의 결과는 -10 입니다.
+//		30과 40의 곱셈의 결과는 1200 입니다.
+//		30과 40의 나눗셈의 결과는  0 입니다.
+		
+		System.out.println(startNum+"과"+endNum+""+"덧셈 결과는"+(startNum + endNum)+"입니다");
+		System.out.println(startNum+"과"+endNum+""+"뻴셈 결과는"+(startNum - endNum)+"입니다");
+		System.out.println(startNum+"과"+endNum+""+"곱셈 결과는"+(startNum * endNum)+"입니다");
+		float dividResult = startNum / endNum;
+		System.out.println(startNum+"과"+endNum+""+"나눗셈 중 몫는"+(int)dividResult+"입니다");
+		System.out.println(startNum+"과"+endNum+""+"나눗셈 결과는"+startNum/(double)endNum+"입니다");
+		System.out.println(startNum+"과"+endNum+""+"덧셈 결과와 뺄셈결과의 곱은"+(startNum + endNum)*(startNum - endNum)+"입니다");
+		
+		
+		
+	}
+
+}
