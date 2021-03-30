aca9e1996b9bd40de67b019d959a13c9d66bfc4e
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-03-30 11:33:43 +0900

    Day2

--- Variable_02/src/com/javalec/base/MainClass.java
+++ Variable_02/src/com/javalec/base/MainClass.java
@@ -4,6 +4,14 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
+//		변수와 상수
+		String str = "James";
+		
+		int num = 10;
+		
+		System.out.println(str + ":" + + num);
+		
+		
 
 	}
 
