9b430a9081eaf0846ddb1b8e95fc08f2f80fc4ed
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-03-31 14:46:39 +0900

    Day 3

--- For/src/com/javalec/base/ForMain.java
+++ For/src/com/javalec/base/ForMain.java
@@ -1,26 +1,67 @@
 package com.javalec.base;
 
+import java.util.*;
+
 public class ForMain {
 
 	public static void main(String[] args) {
 		
 
 		AfterNoonClass anc = new AfterNoonClass();
-		anc.iteratorFor();
+//		anc.iteratorFor();
+		anc.OtoTsum2();
+//		anc.autoSum();
 		
 	}
 
 }
 
 class AfterNoonClass {
+	
+	Scanner sc = new Scanner(System.in);
+	int i;
+		
 	void iteratorFor() {
-		int i;
+		
 		for(i = 0 ; i <=10 ; i ++) {
-			print(i);
+			synPrint(i);
 		}
 		
 	}
-	 void print(int i) {
-		System.out.println(i + " 를 출력합니다.");
+	 void synPrint(int i) {
+		System.out.println(i + " 을(를) 출력합니다.");
+	}
+	 
+	void OtoTsum() {
+		int varSum = 0;
+		
+		for(i = 1 ; i<=50;i++) {
+			varSum = varSum + i;
+		}
+		
+		
+		synPrint(varSum);
+	}
+	void OtoTsum2() {
+		int varSumE = 0;
+		int varSumO = 0;
+		for(i = 1; i <= 100;i++) {
+			if(i % 2 == 0) {
+				varSumE += i;
+			}else {
+				varSumO += i;
+			}
+		}
+	System.out.println("짝수의 합"+varSumE);
+	System.out.println("홀수의 합"+varSumO);	
+	}
+	
+	void autoSum() {
+		int sNum = sc.nextInt();
+		int eNum = sc.nextInt();
+		
+		int totalSum = ((eNum - sNum + 1)*(sNum + eNum))/2;
+		
+		synPrint(totalSum);
 	}
 }
