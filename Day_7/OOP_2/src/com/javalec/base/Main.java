169b0f6c2dfa40cc6cf76cf97129e3052f931d48
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-06 17:44:52 +0900

    Day 7

--- Day_7/OOP_2/src/com/javalec/base/Main.java
+++ Day_7/OOP_2/src/com/javalec/base/Main.java
@@ -1,6 +1,7 @@
 package com.javalec.base;
 
 import com.javalec.function.SumEvenOdd;
+import com.javalec.function.SumEvenOdd2;
 
 public class Main {
 
@@ -10,14 +11,19 @@ public class Main {
 		int i = 1, j = 15;
 		
 		
-		SumEvenOdd seo = new SumEvenOdd();
-		int sum = seo.sumCalc(i , j);
-		
-		String chk = seo.evenOdd(sum);
-		
-		System.out.println(sum);
-		System.out.println(chk);
+//		SumEvenOdd seo = new SumEvenOdd();
+//		int sum = seo.sumCalc(i , j);
+//		
+//		String chk = seo.evenOdd(sum);
+//		
+//		System.out.println(sum);
+//		System.out.println(chk);
 
+		
+		SumEvenOdd2 seo2 = new SumEvenOdd2();
+		
+		seo2.sumCalc(1, 10);
+		seo2.evenOdd();		
 	}
 
 }
---
+++ Day_7/OOP_2/src/com/javalec/function/SumEvenOdd2.java
@@ -0,0 +1,33 @@
+package com.javalec.function;
+
+
+public class SumEvenOdd2 {
+
+	//Field
+	int num = 0 ;
+	
+	//Method (함수)
+	public void sumCalc(int num1, int num2) {
+		int sum = 0;
+		
+		for(int i = num1 ; i <= num2 ; i ++){
+			sum += i;
+		}
+		num = sum;
+		System.out.println(num);
+	}
+	
+	public void evenOdd() {
+		String result;
+		if(num % 2 == 1) {
+			result = "Odd" ;
+		}else {
+			result = "Even";
+		}
+		
+		System.out.println(result);
+	}
+	
+	
+	
+}
